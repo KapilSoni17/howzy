a07d64f2db5acee86ee79fbcb3891c254733d203
KapilSoni17 <sonikapil309@gmail.com>
2021-02-16 13:37:44 +0530

    initial

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,20 @@
+export const environment = {
+  production: true,
+  firebase: {
+    // apiKey: "AIzaSyBnWQYal4hloLpmC5Xd4l5d40TaS0AiSf8",
+    // authDomain: "test-17560.firebaseapp.com",
+    // databaseURL: "https://test-17560.firebaseio.com",
+    // projectId: "test-17560",
+    // storageBucket: "test-17560.appspot.com",
+    // messagingSenderId: "240298821020"
+
+
+    apiKey: "AIzaSyAi_g9smKSDODII-BOZbFet7Wp20j1pI8M",
+    authDomain: "phoneauth-3de75.firebaseapp.com",
+    projectId: "phoneauth-3de75",
+    storageBucket: "phoneauth-3de75.appspot.com",
+    messagingSenderId: "1044516839492",
+    appId: "1:1044516839492:web:c62c8d45d468fd859d1c11",
+    measurementId: "G-W5CQM2VZCQ"
+  }
+};
